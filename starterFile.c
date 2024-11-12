9ee38f7f64dfa26e3069610fbcd93ae1b3a1cf9a
MT6661 <mt666@kent.ac.uk>
2024-11-12 11:12:48 +0000

    part 1

--- starterFile.c
+++ starterFile.c
@@ -1,1 +1,32 @@
-### Your code goes here
+#include <stdio.h>
+
+int main() {
+
+    int num = 42;
+    int *ptr;
+
+    ptr = &num;
+
+    printf("Value of num: %d\n", num);
+    printf("Address of num: %p\n", (void*)&num);
+    printf("Value stored in ptr: %p\n", (void*)ptr);
+
+    *ptr = 100;
+
+    printf("Modified value of num: %d\n", *ptr);
+
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *arrPtr = arr;
+    printf("Original array: \n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", *(arrPtr + i));
+    }
+    printf("\nModified array:\n");
+
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]*=2);
+    }
+
+    return 0;
+}
